c8a0e32445dccb2f42954b94f8df15029e2a7b56
Patrick <d3non@bsc-omnet.(none)>
2013-03-04 13:13:54 +0100

    initial commit

---
+++ darknetsim/src/DarknetBaseNode.cc
@@ -0,0 +1,74 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include <omnetpp.h>
+#include "DarknetBaseNode.h"
+//#include "UDPControlInfo_m.h"
+
+void DarknetBaseNode::initialize(int stage)
+{
+    // because of IPAddressResolver, we need to wait until interfaces are registered,
+    // address auto-assignment takes place etc.
+    // TODO took this from Strufes UDPSampleApp; where do we have to add boottrsap?
+    if (stage!=3)
+        return;
+
+
+    localPort = par("local_port");
+
+    bindToPort(localPort);
+}
+
+
+void DarknetBaseNode::sendPacket(cPacket *pkg)
+{
+//    sendToUDP(pkg, localPort, destAddress, destPort);
+
+}
+
+void DarknetBaseNode::handleMessage(cMessage *msg)
+{
+    if (msg->isSelfMessage())
+    {
+
+        // maybe a timer / internal message handling?
+
+
+
+        // or a packet that we want so send..
+        sendPacket(check_and_cast<cPacket*>(msg));
+    }
+    else
+    {
+        processIncomingPacket(check_and_cast<DarknetMessage*>(msg));
+    }
+
+    if (ev.isGUI())
+    {
+        char buf[40];
+//        sprintf(buf, "rcvd: %d pks\nsent: %d pks", numReceived, numSent);
+        setDisplayString("t=0");
+    }
+}
+
+
+void DarknetBaseNode::processIncomingPacket(DarknetMessage *msg)
+{
+    if (msg->destNodeID != nodeID) {
+        //forward message
+    }
+
+    //distinguish MessageTypes
+}
---
+++ darknetsim/src/DarknetBaseNode.h
@@ -0,0 +1,47 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef DARKNETBASENODE_H_
+#define DARKNETBASENODE_H_
+
+#include <omnetpp.h>
+#include "UDPAppBase.h"
+#include "messages/DarknetMessage.h"
+#include "messages/MaintenanceMessage.h"
+
+class DarknetBaseNode : public UDPAppBase  {
+protected:
+
+    std::string nodeID;
+    int localPort;
+
+    // higher level interface for DarknetNodes
+    virtual void sendMessage(DarknetMessage* msg);
+    virtual void connectPeer(IPvXAddress destAddr, int destPort);
+
+    virtual void handleMaintenanceMessage(MaintenanceMessage* msg);
+    virtual void handleRequestMessage(DarknetMessage* msg);
+
+
+    virtual void handleMessage(cMessage* msg);
+    virtual void processIncomingPacket(DarknetMessage* msg);
+    virtual void sendPacket(cPacket *pkg); // most likely only a wrapper around sendToUDP
+
+
+    virtual int numInitStages() const { return 4; } //TODO: why? why 4?
+    virtual void initialize(int stage);
+};
+
+#endif /* DARKNETBASENODE_H_ */
