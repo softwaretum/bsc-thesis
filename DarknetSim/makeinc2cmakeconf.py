e71ead9f4bcd51eedab538e8dc80c7261d1f4bad
Alexander Hirsch <1zeeky@gmail.com>
2013-04-27 16:44:21 +0200

    makeinc2cmakeconf.py:   add printing usage with -h   ask before overwriting config-file   avoid cmake_set and _add switcheroo ("cmake_op") by always using add and empty initialization   refactored (c)flags-parsing   added LDFLAGS and *_LIBS parsing

--- DarknetSim/makeinc2cmakeconf.py
+++ DarknetSim/makeinc2cmakeconf.py
@@ -2,13 +2,23 @@
 
 from io import open
 import re
+import shlex
 from subprocess import Popen, PIPE
 from sys import argv, exit
 import os
 
+def usage():
+    print(
+        "%s [path to Makefile.inc]" % argv[0],
+        "\tThis tool attempts to (partially, but sufficiently) convert OMNeT++'s Makefile.inc to a CMake config-file.",
+        sep = "\n")
+    exit(0)
+
 # First find OMNeT++'s Makefile.inc.
 makefile_path = None
 if len(argv) > 1:
+    if "-h" in argv:
+        usage()
     makefile_path = argv[1]
 else:
     print("Attempting to locate Makefile.inc.")
@@ -34,13 +44,24 @@ else:
 
 if makefile_path is None:
     print("Could not locate Makefile.inc.")
-    # TODO: ask user to supply path via stdin
-    exit(1)
+    makefile_path = input("Please provide the path to OMNeT++'s Makefile.inc: ")
 
 print("Using \"%s\"." % makefile_path)
 
 # Now create out CMake config file and start writing some generic stuff.
-cmakefile = open("OMNeT++Config.cmake", 'w')
+cmakefile_path = "OMNeT++Config.cmake"
+
+if os.path.exists(cmakefile_path):
+    while True:
+        answer = input("Warning: %s already exists. Proceed? [y/N] " % cmakefile_path).lower()
+        if "no".startswith(answer):
+            exit(0)
+        elif "yes".startswith(answer):
+            break
+
+print("Writing to %s" % cmakefile_path)
+
+cmakefile = open(cmakefile_path, 'w')
 cmakefile.write("# This file was auto-generated via %s\n" % argv[0])
 cmakefile.write("# This CMake config might not expose the complete OMNeT++ build environment\n\n")
 
@@ -48,6 +69,11 @@ print(
     "if(NOT OMNeT++_BUILD_TYPE)",
     "\tset(OMNeT++_BUILD_TYPE \"release\")",
     "endif()",
+    "",
+    "set(OMNeT++_DEFINITIONS)",
+    "set(OMNeT++_INCLUDE_DIRS)",
+    "set(OMNeT++_LIBRARY_DIRS)",
+    "set(OMNeT++_LIBRARIES)",
     sep = "\n", file = cmakefile)
 
 # This controls indentation-level for cmake_set and cmake_add.
@@ -72,20 +98,63 @@ def begin_write():
         cmakefile.write("\n")
     newline = False
 
-# Converts CFLAGS_$MODE to (roughly) if(BUILD_TYPE == $MODE) OMNeT++_DEFINITIONS = CFLAGS
-# Only consideres -D flags, further configuration should be done at project-scope
-# TODO: parse -I, -isystem, -L and -l
-def convert_cflags(match_key, match_value, cmake_op):
+# Splits command-line parameters according to filters,
+# a { "-switch": list }-dict. All switch-values will be appended to list.
+def distribute_parameters(filters, args):
+    # put_into is for key, value in two parameters ("-DDEF" vs "-D DEF")
+    put_into = None
+    for parameter in shlex.split(args):
+        if put_into:
+            put_into.append(parameter)
+            put_into = None
+            continue
+        for switch, into in filters.items():
+            if parameter.startswith(switch):
+                rest = parameter[len(switch):]
+                if rest == "":
+                    put_into = into
+                else:
+                    into.append(rest)
+                continue
+
+# Only consideres -D, -I and -isystem flags, further configuration should be done at project-scope
+def convert_cflags(flags):
+    defs = []
+    includes = []
+    filters = {
+        "-D": defs,
+        # Unlikely, but if there are include-directories defined this way, they'll get added
+        "-I": includes,
+        # Should we differentiate for CMake?
+        "-isystem": includes,
+        # There are many more potentially relevant, I've never seen them in the wild before though.
+    }
+    distribute_parameters(filters, flags)
+    return dict(filter(lambda item: item[1] != [], { "DEFINITIONS": map(lambda str: "-D" + str, defs), "INCLUDE_DIRS": includes }.items()))
+
+def convert_ldflags(flags):
+    lib_dirs = []
+    libs = []
+    filters = {
+        "-L": lib_dirs,
+        "-l": libs,
+    }
+    distribute_parameters(filters, flags)
+    return dict(filter(lambda item: item[1] != [], { "LIB_DIRS": lib_dirs, "LIBRARIES": libs }.items()))
+
+# Converts CFLAGS_$MODE = FLAGS to (roughly) if(BUILD_TYPE == $MODE) OMNeT++_DEFINITIONS = FLAGS
+def per_config_cflags(match_key, match_value):
     global newline
     global cmakefile_indentation
-    # Properly done this would parse the HAVE_*-variables and look for its CMake modules.
-    # meh
-    configuration = key[7:].lower()
-    defs = filter(lambda flag: flag.startswith("-D"), value.split())
+    # match_key = "CFLAGS_*", strip CFLAGS_
+    configuration = match_key[7:]
+    flags = convert_cflags(match_value)
+    
     begin_write()
     cmakefile.write("if(${OMNeT++_BUILD_TYPE} STREQUAL \"%s\")\n" % configuration)
     cmakefile_indentation += 1
-    cmake_op("DEFINITIONS", " ".join(defs))
+    for name, values in flags.items():
+        cmake_add(name, " ".join(values))
     cmakefile_indentation -= 1
     cmakefile.write("endif()\n")
     newline = True
@@ -119,9 +188,6 @@ translate = {
     "MSGC": "MSGC",
 }
 
-# This is for CPPFLAGS, whether we need to append or assign them to DEFINITIONS
-defs_set = False
-
 # Now we start converting the Makefile.inc
 for linenumber, line in enumerate(open(makefile_path, 'r')):
     if regex_empty.match(line):
@@ -142,16 +208,28 @@ for linenumber, line in enumerate(open(makefile_path, 'r')):
                 cmake_set("VERSION_" + version_parts[index], version)
             cmake_set("VERSION_STRING", value)
             newline = True
+        # Properly done this would parse the HAVE_*-variables and look for its CMake modules.
+        # meh
         elif key.startswith("CFLAGS_"):
-            cmake_op = cmake_set if defs_set != 'cppflags' else cmake_add
             newline = True
-            convert_cflags(key, value, cmake_op)
-            defs_set = defs_set or 'cflags'
+            per_config_cflags(key, value)
         elif key == "CPPFLAGS":
-            cmake_op = cmake_set if defs_set != 'cflags' else cmake_add
             begin_write()
-            cmake_op("DEFINITIONS", value)
-            defs_set = defs_set or 'cppflags'
+            for name, values in convert_cflags(value).items():
+                cmake_add(name, " ".join(values))
+        elif key == "LDFLAGS":
+            begin_write()
+            for name, values in convert_ldflags(value).items():
+                cmake_add(name, " ".join(values))
+            # These needs a bit more hackaroo
+        elif key.endswith("ENV_LIBS") or key == "KERNEL_LIBS":
+            begin_write()
+            libs = filter(lambda flag: flag.startswith("-l"), shlex.split(value))
+            # remove -l and $D from the flags
+            libs = list(map(lambda libflag: libflag[2:-2], libs))
+            if libs == []:
+                continue
+            cmake_add("LIBRARIES", " ".join(libs))
         continue
     
     match = regex_append.search(line)
@@ -164,7 +242,14 @@ for linenumber, line in enumerate(open(makefile_path, 'r')):
         elif key == "OMNETPP_VERSION":
             print("Sorry, I can't deal with adding to OMNETPP_VERSION (yet?)\n\tfile %s, line %d: %s\n\tIgnoring..." % (makefile_path, linenumber, line.strip()))
         elif key.startswith("CFLAGS_"):
-            convert_cflags(key, value, cmake_add)
+            newline = True
+            per_config_cflags(key, value)
+        elif key == "CPPFLAGS":
+            begin_write()
+            for name, values in convert_cflags(value).items():
+                cmake_add(name, " ".join(values))
+        elif key == "LDFLAGS":
+            begin_write()
+            for name, values in convert_ldflags(value).items():
+                cmake_add(name, " ".join(values))
         continue
-    
-    # TODO: _LIBS
